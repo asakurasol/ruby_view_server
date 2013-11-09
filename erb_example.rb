d8091a7618ca4e6a4d4d14a41dd8a94f646d9d58
asakurasol <mamamia1734@GMAIL.COM>
2013-11-09 13:35:26 -0800

    added a loop code

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,11 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+counter = 1
+template = ERB.new "watch this <%= 
+while counter < 11
+  puts counter
+  counter = counter + 1
+end%>"
+puts template.result(binding)
